5c1857e1069de414a3d78fa2f6c24e5d65c303d3
z5882852 <86944419+z5882852@users.noreply.github.com>
2024-08-24 23:46:38 +0800

    feat(login): 新增扫码登录

--- src/page/login/Login.vue
+++ src/page/login/Login.vue
@@ -1,19 +1,32 @@
 <template>
     <el-main class="login-container card" v-loading="loading">
         <h1 class="login-title">学生成果管理系统后台</h1>
-        <el-form :model="form" label-width="auto" style="max-width: 600px">
-            <el-space fill>
-                <el-alert type="info" show-icon :closable="false">
-                    <p>当前为测试环境，暂不支持用户名登录，请使用token登录</p>
-                </el-alert>
-                <el-form-item label="token">
-                    <el-input v-model="form.token" />
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="this.setToken" class="btn">提交</el-button>
-                </el-form-item>
-            </el-space>
-        </el-form>
+        <div v-if="showQR">
+            <div class="qrcode-container">
+                <el-avatar shape="square" :size="200" fit="fill" :src="qrcode.base64" v-loading="qrcode.loading" />
+                <el-text class="mx-1" type="info">请使用微信扫码</el-text>
+            </div>
+            <el-button class="btn-toggleQR" type="primary" @click="toggleQR" link>Token登录</el-button>
+        </div>
+        <div v-else>
+            <el-form :model="form" label-width="auto" style="max-width: 600px">
+                <el-space fill>
+                    <el-alert type="info" show-icon :closable="false">
+                        <p>当前为测试环境，暂不支持用户名登录，请使用token登录</p>
+                    </el-alert>
+                    <el-form-item label="token">
+                        <el-input v-model="form.token" />
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="this.setToken" class="btn">提交</el-button>
+                    </el-form-item>
+                </el-space>
+            </el-form>
+            <el-button class="btn-toggleQR" type="primary" @click="toggleQR" link>扫码登录</el-button>
+        </div>
+
+
+
     </el-main>
 </template>
 
@@ -28,9 +41,16 @@ export default {
     name: 'Login',
     data() {
         return {
+            showQR: false,
             loading: false,
             form: {
-                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwib3BlbmlkIjoib1pid1g3Y1p1bkZkbDVQc25aM3FCbklhUlRVRSIsImV4cCI6MTcyMzgyNzU0OX0.I6dpdVQx4HZO1uz9p7xJ9xYqZmY3QADb232uLlW5Opo",
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwib3BlbmlkIjoib1pid1g3Y1p1bkZkbDVQc25aM3FCbklhUlRVRSIsImV4cCI6MTcyOTIwMTg4Mn0.C4Qh2PCcjVN2sdHNrhEadBK8Q8Wa5CmzvopZttZLTtE",
+            },
+            qrcode: {
+                loading: true,
+                clentId: '',
+                base64: '',
+                intervalId: null,
             }
         }
     },
@@ -93,11 +113,101 @@ export default {
                 console.error(error)
             })
 
-        }
+        },
+        getQRCode() {
+            this.qrcode.loading = true;
+            window.$axios.get("/user/qrcode/create").then(response => {
+                if (response.status == 200) {
+                    if (response.data.code == 200) {
+                        let data = response.data.data
+                        this.qrcode.base64 = "data:image/png;base64," + data.image;
+                        this.qrcode.clentId = data.client_id;
+                        this.startCheck();
+                    } else {
+                        ElMessage({
+                            message: `获取二维码失败，${response.data.message}`,
+                            type: 'error',
+                        })
+                    }
+                } else {
+                    ElMessage({
+                        message: `获取二维码失败，: ${response.status}`,
+                        type: 'error',
+                    })
+                }
+                this.qrcode.loading = false;
+            }).catch(error => {
+                this.qrcode.loading = false;
+                ElMessage({
+                    message: `获取二维码失败，请检查网络`,
+                    type: 'error',
+                })
+                console.error(error)
+            })
+        },
+        startCheck() {
+            if (this.qrcode.intervalId) {
+                clearInterval(this.qrcode.intervalId);
+            }
+            this.qrcode.intervalId = setInterval(this.checkQRCodeStatus, 1000);
+        },
+        checkQRCodeStatus() {
+            window.$axios.post("/user/qrcode/status", { client_id: this.qrcode.clentId }).then(response => {
+                if (response.status == 200) {
+                    if (response.data.code == 200) {
+                        let data = response.data.data
+                        if (data.status === 'ready') {
+                            return
+                        }
+                        localStorage.setItem("access_token", data.access_token);
+                        ElMessage({
+                            message: `登录成功!`,
+                            type: 'success',
+                        })
+                        this.setFlag();
+                    } else {
+                        ElMessage({
+                            message: `二维码登录发生错误，${response.data.message}`,
+                            type: 'error',
+                        })
+                    }
+                } else {
+                    ElMessage({
+                        message: `二维码登录发生错误，: ${response.status}`,
+                        type: 'error',
+                    })
+                }
+                if (this.qrcode.intervalId) {
+                    clearInterval(this.qrcode.intervalId);
+                }
+            }).catch(error => {
+                ElMessage({
+                    message: `发生错误，请检查网络`,
+                    type: 'error',
+                })
+                console.error(error)
+                if (this.qrcode.intervalId) {
+                    clearInterval(this.qrcode.intervalId);
+                }
+            })
+        },
+        toggleQR() {
+            this.showQR = !this.showQR
+            if (this.showQR) {
+                this.getQRCode()
+            }
+        },
     },
     setup() {
         const isDark = useDark()
         const toggleDark = useToggle(isDark)
+
+    },
+    beforeUnmount() {
+        // 组件卸载时清除定时器
+        if (this.qrcode.intervalId) {
+            clearInterval(this.qrcode.intervalId);
+        }
     }
 }
 </script>
@@ -123,6 +233,7 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    text-align: center;
 }
 
 .card {
@@ -138,5 +249,18 @@ export default {
 
 .btn {
     margin: 0 auto;
+}
+
+.btn-toggleQR {
+    margin: 0 auto;
+    margin-top: 30px;
+    text-align: center;
+
+}
+
+.qrcode-container {
+    margin: 0 auto;
+    width: 200px;
+    height: 200px;
 }
 </style>
